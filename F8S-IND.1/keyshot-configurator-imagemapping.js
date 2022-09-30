de71ebf1a835e31f3daaf1d00c1e7a9d0382223f
mikey45097 <91089553+mikey45097@users.noreply.github.com>
2022-09-30 11:15:02 +0530

    deshbhakti

---
+++ F8S-IND.1/keyshot-configurator-imagemapping.js
@@ -0,0 +1,91 @@
+imagemappingdata =
+[
+    {
+        "image": "F8S-IND.1.1.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "43+40",
+            "48+45"
+        ],
+        "studio": 985360900
+    },
+    {
+        "image": "F8S-IND.1.2.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "43+39",
+            "48+44"
+        ],
+        "studio": 985360900
+    },
+    {
+        "image": "F8S-IND.1.3.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "43+41",
+            "48+46"
+        ],
+        "studio": 985360900
+    },
+    {
+        "image": "F8S-IND.1.4.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "43+42",
+            "48+47"
+        ],
+        "studio": 985360900
+    },
+    {
+        "image": "F8S-IND.1.5.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "43+40",
+            "48+45"
+        ],
+        "studio": 985360901
+    },
+    {
+        "image": "F8S-IND.1.6.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "43+39",
+            "48+44"
+        ],
+        "studio": 985360901
+    },
+    {
+        "image": "F8S-IND.1.7.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "43+41",
+            "48+46"
+        ],
+        "studio": 985360901
+    },
+    {
+        "image": "F8S-IND.1.8.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "43+42",
+            "48+47"
+        ],
+        "studio": 985360901
+    }
+];
---
+++ F8S-IND.1/keyshot-configurator-modeldata.js
@@ -0,0 +1,67 @@
+model =
+{
+    "materialgroups": [
+        {
+            "materials": [
+                {
+                    "icon": "stripe%201.png",
+                    "multimaterials": [
+                        "43+40",
+                        "48+45"
+                    ],
+                    "name": "Stripe 1"
+                },
+                {
+                    "icon": "stripe%202.png",
+                    "multimaterials": [
+                        "43+39",
+                        "48+44"
+                    ],
+                    "name": "Stripe 2"
+                },
+                {
+                    "icon": "stripe%203.png",
+                    "multimaterials": [
+                        "43+41",
+                        "48+46"
+                    ],
+                    "name": "Stripe 3"
+                },
+                {
+                    "checked": true,
+                    "icon": "stripe%204.png",
+                    "multimaterials": [
+                        "43+42",
+                        "48+47"
+                    ],
+                    "name": "Stripe 4"
+                }
+            ],
+            "name": "Material set 1"
+        }
+    ],
+    "products": [
+        {
+            "checked": true,
+            "icon": "default.png",
+            "materialgroups": [
+                "Material set 1"
+            ],
+            "modelset": 1005,
+            "name": "Default"
+        }
+    ],
+    "studios": [
+        {
+            "checked": true,
+            "icon": "front.png",
+            "id": 985360900,
+            "name": "Front"
+        },
+        {
+            "icon": "rear.png",
+            "id": 985360901,
+            "name": "Rear"
+        }
+    ]
+};
